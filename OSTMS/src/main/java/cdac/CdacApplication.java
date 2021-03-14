5852ae4c8994a1864096224f19789b7c400e4a11
anshul shakya <anshulshakya.as6@gmail.com>
2021-03-14 19:58:30 +0530

    sprint-1 back-end tested

--- OSTMS/src/main/java/cdac/CdacApplication.java
+++ OSTMS/src/main/java/cdac/CdacApplication.java
@@ -25,8 +25,6 @@ public class CdacApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("hello");
 
-		// ------------------------ //
-
 	}
 
 }
