04bae60e3e9d576d9e26f2f7c4b21deac3b36fbf
anshul shakya <anshulshakya.as6@gmail.com>
2021-04-12 12:09:41 +0530

    ok

--- OSTMS/src/test/java/cdac/Service1Test.java
+++ OSTMS/src/test/java/cdac/Service1Test.java
@@ -2,6 +2,7 @@ package cdac;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,8 +15,17 @@ public class Service1Test {
 	@Autowired
 	Service s1;
 
-	@Test // testing student login
-	public void loginte() {
-		assertEquals(true, s1.login(1010520001, "anshul66"));
+	// TESTING NOTIFICATIONS
+
+	@DisplayName("Adding Public Notification")
+	@Test
+	public void addnoti() {
+		assertEquals(true, s1.addgnoti("Testing"));
+	}
+
+	@DisplayName("When adding empty/null notification")
+	@Test
+	public void addnoti2() {
+		assertEquals(false, s1.addgnoti(null));
 	}
 }
